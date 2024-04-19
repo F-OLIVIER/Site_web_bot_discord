16d3f44b60e24f417eac6b7f1156845ce39246ed
folivier <fabien.olivier2@gmail.com>
2024-04-19 13:56:04 +0200

    nettoyage

--- screen.sh
+++ screen.sh
@@ -9,6 +9,11 @@ cd ./services/site
 go run ./cmd/main.go
 # CTRL + A puis d pour detacher
 
+# https://caddyserver.com/docs/install
+screen -R caddy
+caddy
+# CTRL + A puis d pour detacher
+
 
 # Afficher les screens en cours
 screen -ls
